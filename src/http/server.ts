77732a6a39d828665ec28b7238f6f03d9ca5bf4c
Messias <fmessiasca@gmail.com>
2024-06-14 08:24:48 -0400

    Instalação e configuração do Docker e do Prisma

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,9 +3,9 @@ import fastify from 'fastify';
 const app = fastify();
 
 app.get('/hello', () => {
-    return "Fala familia!"
+    return "Fala man!"
 })
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server running!')
-})
+});
