94400c542b83390b2598e18fef871f9e492f1d3f
Messias <fmessiasca@gmail.com>
2024-06-17 15:47:26 -0400

    Estrutura completa de votação, mudança de voto e aviso de voto repetido

--- src/routes/vote-on-poll.ts
+++ src/routes/vote-on-poll.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { randomUUID, setEngine } from "node:crypto"
 import { prisma } from '../lib/prisma';
 import { FastifyInstance } from 'fastify';
 
@@ -10,10 +11,52 @@ export async function voteOnPoll(app: FastifyInstance) {
 
         const voteOnPollParams = z.object({
             pollId: z.string().uuid(),
-        })
+        });
 
         const { pollId } = voteOnPollParams.parse(request.params);
         const { pollOptionId } = voteOnPollBody.parse(request.body);
+
+        let { sessionId } = request.cookies;
+
+        if (sessionId) {
+            const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+                where: {
+                    sessionId_pollId: {
+                        sessionId,
+                        pollId,
+                    }
+                }
+            })
+
+            if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+                await prisma.vote.delete({
+                    where: {
+                        id: userPreviousVoteOnPoll.id,
+                    }
+                })
+            } else if (userPreviousVoteOnPoll) {
+                return reply.status(400).send({ message: "Você já votou nessa enquete!" })
+            }
+        }
+
+        if (!sessionId) {
+            sessionId = randomUUID();
+
+            reply.setCookie('sessionId', sessionId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                signed: true,
+                httpOnly: true,
+            });
+        }
+
+        await prisma.vote.create({
+            data: {
+                sessionId,
+                pollId,
+                pollOptionId,
+            }
+        })
 
         return reply.status(201).send();
     });
